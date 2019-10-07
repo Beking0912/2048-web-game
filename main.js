f2070d3163ac4af43cc061eb362a2b02c47a5276
Beking <sbqoo@qq.com>
2019-10-07 16:20:40 +0800

    颜色设置

--- main.js
+++ main.js
@@ -17,4 +17,40 @@ function init() {
       cell.css("top", getPosTop(i, j));
       cell.css("left", getPosLeft(i, j));
     }
+
+  // 使 board 成为一个二维数组
+  for (var i = 0; i < 4; i++) {
+    board[i] = new Array();
+    for (var j = 0; j < 4; j++) {
+      board[i][j] = 0;
+    }
+  }
+
+  updateBoardView();
+}
+
+function updateBoardView() {
+  $(".number-cell").remove(); // 移除所有已存在的 number-cell 元素
+  for (var i = 0; i < 4; i++)
+    for (var j = 0; j < 4; j++) {
+      $("#container").append(
+        `<div class="number-cell" id="number-cell-'+i+'-'+j+'"></div>`
+      );
+      var theNumberCell = $("#number-cell-" + i + "-" + j); // 操作当前坐标下的 number-cell 的值
+
+      if (board[i][j] === 0) {
+        theNumberCell.css("width", "0px");
+        theNumberCell.css("height", "0px");
+        theNumberCell.css("top", getPosTop(i, j) + 50);
+        theNumberCell.css("left", getPosLeft(i, j) + 50);
+      } else {
+        theNumberCell.css("width", "100px");
+        theNumberCell.css("height", "100px");
+        theNumberCell.css("top", getPosTop(i, j));
+        theNumberCell.css("left", getPosLeft(i, j));
+        theNumberCell.css("background-color", getNumberBgc(board[i][j]));
+        theNumberCell.css("color", getNumberColor(board[i][j]));
+        theNumberCell.text(board[i][j]);
+      }
+    }
 }
--- support.js
+++ support.js
@@ -5,3 +5,48 @@ function getPosTop(i, j) {
 function getPosLeft(i, j) {
   return 20 + 120 * j;
 }
+
+function getNumberBgc(number) {
+  switch (number) {
+    case 2:
+      return "#e3f2fd";
+      break;
+    case 4:
+      return "#bbdefb";
+      break;
+    case 8:
+      return "#90caf9";
+      break;
+    case 16:
+      return "#64b5f6";
+      break;
+    case 32:
+      return "#42a5f5";
+      break;
+    case 64:
+      return "#2196f3";
+      break;
+    case 128:
+      return "#1e88e5";
+      break;
+    case 256:
+      return "#1976d2";
+      break;
+    case 512:
+      return "#1565c0";
+      break;
+    case 1024:
+      return "#0d47a1";
+      break;
+    case 2048:
+      return "#1a237e";
+      break;
+    // case 4096:return 'rgb(240, 201, 207)';break;
+    // case 8192:return 'rgb(240, 201, 207)';break;
+  }
+}
+
+function getNumberColor(number) {
+  if (number <= 16) return "#0d47a1";
+  return "white";
+}
